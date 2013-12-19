da0ccdf27bf876c758c0c5c9f4388c398bbdf5bb
s1341 <github@shmarya.net>
2013-12-19 21:48:33 +0200

    add vimrcs and install script

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+function realpath {
+    pushd $(dirname $1) > /dev/null
+    local res=$(pwd)
+    popd > /dev/null
+    echo "$res/$1"
+}
+
+function install_file {
+    local source="$1"
+    local target="$2"
+    if [[ -e $target ]]
+    then
+        if [[ -L $target ]]
+        then
+            rm $target
+        else
+            mv $target $target.$(date +'%Y%m%d_%H%M');
+        fi
+    fi
+    ln -s $(realpath $source) $target
+}
+
+
+
+# install vimrc files, meant to be used with spf13
+for file in vimrc*
+do
+    install_file $file "$HOME/.$file"
+done
+
+# install spf13 vim distribution as base, the bundles specified
+# in the vimrc.bundles.local file will automatically be installed
+curl https://j.mp/spf13-vim3 -L -o - | sh
+
+# build native for YouCompleteMe and vimproc
+(cd $HOME/.vim/bundle/vimproc; make clean all)
+(cd $HOME/.vim/bundle/YouCompleteMe; ./install.sh --clang-completer --system-libclang)
