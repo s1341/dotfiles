fc56ff58b200e1664ac992a61de190143502615a
s1341 <github@shmarya.net>
2013-12-28 18:45:51 +0200

    update install.sh to install git-crypt

--- install.sh
+++ install.sh
@@ -23,11 +23,30 @@ function install_file {
     ln -s $(realpath $source) $target
 }
 
-
+echo "[>] starting install"
 # switch to the directory of this script
 pushd $(dirname $0) > /dev/null
 
-# install vimrc files, meant to be used with spf13
+# convert ssh public key to openssl compatible PEM
+# we use this PEM to encrypt some sensitive files in a pre-commit hook
+if [[ ! -e id_rsa.pem.pub ]]
+then
+    echo "[*] exporting public key"
+    ssh-keygen -f ~/.ssh/id_rsa.pub -e -m PKCS8 > id_rsa.pub.pem
+fi
+
+if [[ ! -e modules/git-crypt/git-crypt ]]
+then
+    # install git-crypt
+    echo "[*] installing git-crypt"
+    git submodule init && git submodule update
+    pushd modules/git-crypt > /dev/null
+    make > /dev/null
+    chmod +x git-crypt
+    popd > /dev/null
+fi
+
+echo "[*] install vimrc files, meant to be used with spf13"
 for file in *vimrc*
 do
     install_file $file "$HOME/.$file"
@@ -35,19 +54,21 @@ done
 
 # install spf13 vim distribution as base, the bundles specified
 # in the vimrc.bundles.local file will automatically be installed
-curl https://j.mp/spf13-vim3 -L -o - | sh
+# TODO: don't do the full install every time
+echo "[*] install spf13"
+#curl https://j.mp/spf13-vim3 -L -o - | sh
 
-# build native for YouCompleteMe and vimproc
+echo "[*] build native for YouCompleteMe and vimproc"
 (cd $HOME/.vim/bundle/vimproc; make clean all)
 (cd $HOME/.vim/bundle/YouCompleteMe; ./install.sh --clang-completer --system-libclang)
 
-# install Xdefaults
+echo "[*] install Xdefaults"
 install_file Xdefaults "$HOME/.Xdefaults"
 
-# install ssh config
+echo "[*] install ssh config"
 install_file ssh_config "$HOME/.ssh/config"
 
-# xmonad
+echo "[*] installing xmonad.hs"
 [[ -d $HOME/.xmonad ]] || mkdir -p $HOME/.xmonad
 install_file xmonad.hs "$HOME/.xmonad/xmonad.hs"
 
