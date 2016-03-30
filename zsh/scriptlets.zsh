6704e9197a2eaa135d465927fa71e2139e8fd0a6
s1341 <github@shmarya.net>
2016-03-30 14:14:17 +0300

    Some updates to zsh config

--- zsh/scriptlets.zsh
+++ zsh/scriptlets.zsh
@@ -2,3 +2,13 @@ function find_large_files {
   count=${1:=10}
   find . -type f -print0 | xargs -0 du | sort -n | tail -$count | cut -f2 | xargs -I{} du -sh {}
 }
+
+function hardtouch {
+  tail -n1 "$1" | grep 'TOUCHPATTERN' >/dev/null || echo //TOUCHPATTERN0 >> "$1"
+
+  sed -i 's/TOUCHPATTERN[0-9]*/TOUCHPATTERN'$(date +%H%M%S)'/' "$1"
+}
+
+function unhardtouch {
+  sed -i '/TOUCHPATTERN/d' "$1"
+}
